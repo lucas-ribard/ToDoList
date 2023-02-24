fafc585e035dbe4d9c964f0551ce907bb90bf4c9
lucas-ribard <lucas.ribard@laplateforme.io>
2023-02-24 14:56:53 +0100

    bugfix

--- V1/script.js
+++ V1/script.js
@@ -10,65 +10,65 @@ function createNewTaskElement(task) {
     console.log("Construction de l'élément :");
 
     //si task n'est pas vide 
-    if(task){
-    var TaskElement = document.createElement("li");//nouvel élément de liste
+    if (task) {
+        var TaskElement = document.createElement("li");//nouvel élément de liste
 
-    var checkBox = document.createElement("input");//input checkbox
-    checkBox.type = "checkbox";//passe l'input en type checkbox
-    checkBox.className = "checkbox";
+        var checkBox = document.createElement("input");//input checkbox
+        checkBox.type = "checkbox";//passe l'input en type checkbox
+        checkBox.className = "checkbox";
 
-    var label = document.createElement("label");//nom de la tache
-    label.innerText = task; //ecrit la tache dans <label>
+        var label = document.createElement("label");//nom de la tache
+        label.innerText = task; //ecrit la tache dans <label>
 
-    var editInput = document.createElement("input");//zone de text d'edit
-    editInput.type = "text";
+        var editInput = document.createElement("input");//zone de text d'edit
+        editInput.type = "text";
 
-    var editButton = document.createElement("button");//bt edit
-    editButton.innerText = "Edit";//innerText encodes special characters, HTML does not.
-    editButton.className = "edit";
+        var editButton = document.createElement("button");//bt edit
+        editButton.innerText = "Edit";//innerText encodes special characters, HTML does not.
+        editButton.className = "edit";
 
-    var deleteButton = document.createElement("button");//bt suppr
-    deleteButton.innerText = "Delete";
-    deleteButton.className = "delete";
-    
-    var br1 = document.createElement('br');
-    var br2 = document.createElement('br');
-    var Ligne = document.createElement('hr');
-   
-    let DateHour = runClock();
-    let Date = document.createElement("date");
-    Date.innerText = DateHour;
+        var deleteButton = document.createElement("button");//bt suppr
+        deleteButton.innerText = "Delete";
+        deleteButton.className = "delete";
 
-    //crée l'élément et le retourne
-    //<li><input type="checkbox"><label>TASK</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>
-    TaskElement.appendChild(checkBox);
-    TaskElement.appendChild(label);
-    TaskElement.appendChild(editInput);
-    TaskElement.appendChild(editButton);
-    TaskElement.appendChild(deleteButton);
-    TaskElement.appendChild(Date);
-    TaskElement.appendChild(br1);
-    TaskElement.appendChild(br2);
-    TaskElement.appendChild(Ligne);
-   
-    console.log(TaskElement);
-    return TaskElement;
+        var br1 = document.createElement('br');
+        var br2 = document.createElement('br');
+        var Ligne = document.createElement('hr');
+
+        let DateHour = runClock();
+        let Date = document.createElement("date");
+        Date.innerText = DateHour;
+
+        //crée l'élément et le retourne
+        //<li><input type="checkbox"><label>TASK</label><input type="text"><button class="edit">Edit</button><button class="delete">Delete</button></li>
+        TaskElement.appendChild(checkBox);
+        TaskElement.appendChild(label);
+        TaskElement.appendChild(editInput);
+        TaskElement.appendChild(editButton);
+        TaskElement.appendChild(deleteButton);
+        TaskElement.appendChild(Date);
+        TaskElement.appendChild(br1);
+        TaskElement.appendChild(br2);
+        TaskElement.appendChild(Ligne);
+
+        console.log(TaskElement);
+        return TaskElement;
+    }
 }
-}
 
-function runClock(){
-    let today   = new Date();
-    let timeValue   = today.getHours();
+function runClock() {
+    let today = new Date();
+    let timeValue = today.getHours();
     let minutes = today.getMinutes();
     let dateValue = today.toLocaleDateString();
- 
+
     // Si le nombre de minutes est inférieur à 10, alors on ajoute un 0 devant sinon ca donne (10h2 plutot que 10h02)
     timeValue += ((minutes < 10) ? ":0" : ":") + minutes;
-    DateHour=timeValue;
-    DateHour+= " ";
-    DateHour+= dateValue;
-   return DateHour;
-  }
+    DateHour = timeValue;
+    DateHour += " ";
+    DateHour += dateValue;
+    return DateHour;
+}
 
 //crée la nouvelle tache a partir de l'élément crée
 function NouvelleTask() {
@@ -81,7 +81,7 @@ function NouvelleTask() {
     bindTaskEvents(TaskElement, taskCompleted);
 
     NewTask.value = "";
-    console.log("Tache :",TaskElement.querySelector("label").innerText,"crée");
+    console.log("Tache :", TaskElement.querySelector("label").innerText, "crée");
 }
 //Quand le bouton Add est cliqué, il active la fonction NouvelleTask
 ButtonAdd.onclick = NouvelleTask;
@@ -98,13 +98,13 @@ function deleteTask() {
 //Editer une tache
 function editTask() {
     console.log("Edit Task :", this.parentNode.querySelector("label").innerText);
-   
+
     //si la classe est .editmode
     if (this.parentNode.classList.contains("editMode")) {
         //switch to .editmode
         //label devient l'input.
         this.parentNode.querySelector("label").innerText = this.parentNode.querySelector('input[type=text]').value;
-        
+
     } else {
         //sinon l'input devient le label
         this.parentNode.querySelector('input[type=text]').value = this.parentNode.querySelector("label").innerText;
@@ -113,9 +113,9 @@ function editTask() {
     //toggle .editmode sur le parent parent.
     this.parentNode.classList.toggle("editMode");
 
-    if(this.parentNode.querySelector("button").innerText == "Edit"){
+    if (this.parentNode.querySelector("button").innerText == "Edit") {
         this.parentNode.querySelector("button").innerText = "Save";
-    }else{
+    } else {
         this.parentNode.querySelector("button").innerText = "Edit"
     }
 }
@@ -125,10 +125,10 @@ function editTask() {
 function taskCompleted() {
     console.log("Task", this.parentNode.querySelector("label").innerText, "is now Completed");
 
-    let DateHour=runClock();
+    let DateHour = runClock();
     this.parentNode.querySelector("date").innerText = DateHour;
 
-    //Append the task list item to the #completed-tasks
+    //Append task list item to  completed-tasks
     completeTask.appendChild(this.parentNode);
     bindTaskEvents(this.parentNode, taskIncomplete);
 
@@ -155,7 +155,7 @@ function bindTaskEvents(taskListItem, checkBoxEventHandler) {
     taskListItem.querySelector("input[type=checkbox]").onchange = checkBoxEventHandler;
 }
 
-//cycle over incompleteTask ul list items
+//cycle over incompleteTask
 
 //for each list item
 for (var i = 0; i < incompleteTask.children.length; i++) {
@@ -164,9 +164,9 @@ for (var i = 0; i < incompleteTask.children.length; i++) {
 }
 
 //cycle over completedTasksHolder ul list items
-for (var i=0; i<completeTask.children.length;i++){
-	//bind events to list items chldren(tasksIncompleted)
-		bindTaskEvents(completeTask.children[i],taskIncomplete);
-	}
+for (var i = 0; i < completeTask.children.length; i++) {
+    //bind events to list items chldren(tasksIncompleted)
+    bindTaskEvents(completeTask.children[i], taskIncomplete);
+}
 
 
